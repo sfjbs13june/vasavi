a9f096610d124fed8f20956940baced169535f49
vasavi <vasavi.pitta@tcs.com>
2023-01-23 16:01:54 +0000

    day 06 integration test commit

--- day06/patient-app/src/main/java/com/vasavi/app/controller/PatientController.java
+++ day06/patient-app/src/main/java/com/vasavi/app/controller/PatientController.java
@@ -4,15 +4,20 @@ package com.vasavi.app.controller;
 import com.vasavi.app.model.Patient;
 import com.vasavi.app.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PatientController {
 
+    @Value("${app.name}")
+    String name;
     @Autowired
     PatientService patientService;
     @PostMapping("/patient/save")
     public Patient savePatient(@RequestParam Patient patient){
+
+        System.out.println(name);
         System.out.println(patient);
         return patient;
     }
