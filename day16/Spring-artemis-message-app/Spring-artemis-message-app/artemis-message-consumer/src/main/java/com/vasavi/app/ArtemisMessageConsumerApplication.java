b7880da9990a391657ce5a68d5053efd551b8ff7
vasavi <vasavi.pitta@tcs.com>
2023-02-02 16:15:45 +0000

    day 16

---
+++ day16/Spring-artemis-message-app/Spring-artemis-message-app/artemis-message-consumer/src/main/java/com/vasavi/app/ArtemisMessageConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.vasavi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtemisMessageConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtemisMessageConsumerApplication.class, args);
+	}
+
+}
---
+++ day16/Spring-artemis-message-app/Spring-artemis-message-app/src/main/java/com/vasavi/app/SpringArtemisMessageAppApplication.java
@@ -0,0 +1,13 @@
+package com.vasavi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringArtemisMessageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringArtemisMessageAppApplication.class, args);
+	}
+
+}
